3d1da00184a996cedc1fa9638fd3e86678ee76a8
lioneer32232002-commits <lioneer32232002-commits@users.noreply.github.com>
2026-05-26 17:21:40 +0800

    feat: add assembly definitions and UTF test infrastructure

---
+++ Assets/Tests/EditMode/ScoringCalculatorTests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+using VirtuaCop2;
+
+namespace VirtuaCop2.Tests
+{
+    public class ScoringCalculatorTests
+    {
+        [Test]
+        public void Placeholder_AlwaysPasses()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}

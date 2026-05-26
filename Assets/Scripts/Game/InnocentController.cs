8c5a73750abf83dedffcf846692e6ad6aa839ba2
lioneer32232002-commits <lioneer32232002-commits@users.noreply.github.com>
2026-05-26 17:29:46 +0800

    feat: add InnocentController with damage penalty on shot

---
+++ Assets/Scripts/Game/InnocentController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using UnityEngine;
+
+namespace VirtuaCop2
+{
+    public class InnocentController : MonoBehaviour
+    {
+        [SerializeField] private float visibleDuration = 3f;
+
+        public bool IsAlive { get; private set; } = true;
+
+        public void Emerge()
+        {
+            IsAlive = true;
+            gameObject.SetActive(true);
+            StartCoroutine(AppearSequence());
+        }
+
+        private IEnumerator AppearSequence()
+        {
+            yield return new WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(visibleDuration);
+            Flee();
+        }
+
+        private void Flee()
+        {
+            IsAlive = false;
+            gameObject.SetActive(false);
+        }
+
+        public void OnShot()
+        {
+            if (!IsAlive) return;
+            IsAlive = false;
+            StopAllCoroutines();
+            PlayerController.Instance?.TakeDamage(1);
+            HUDManager.Instance?.TriggerInnocentFlash();
+            ScoringSystem.Instance?.ResetCombo();
+            gameObject.SetActive(false);
+        }
+    }
+}
